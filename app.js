9b75e285005e1459137b41f3adc046e74bc5160d
Pablo Estrella P <pablolili5370@gmail.com>
2022-03-06 12:31:38 -0500

    Credenciales Heroku cambiadas

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ const port = process.env.PORT || 3000;
 //Conexion a Base de Datos
 const mongo = require('mongoose');
 
-/*Las credenciales de USER,PASSWORD y DBNAME las lee del archivo .env que esta en esta maquina
-  Para que funcione al desplegar en heroku debemos estas mismas credenciales configurarlas en el
-  hosting que es heroku para esto el archivo .env tiene que ser ignorado en .gitignore esto
-  significa que cuando se despliega este proyecto en heroku las variables de entorno no son subidas
-  al hosting y debemos estas mismas variables de .env configurarlas en heroku*/
 const uri= `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.gzlj2.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 //bQffatxgzNeLCSEZ
 mongo.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true})
@@ -28,20 +23,7 @@ app.set('views', __dirname + '/views') //Le dice que las vistas(views) o archivo
 //_dirname es la ruta del servidor donde estan app.js, package.json. Es importante este _dirname por que cuando
 //la pagina se aloje en un hosting no se perdera d ela rura del servidor.
 app.use(http.static(__dirname + '/public')); //Esta es la ruta de archivos estaticos(carpeta public) que son las paginas html, CSS y codigo script
-/* Para paginas dinamicas que son aquellas que deben refrescarse despues de una consulta a base de datos
-se usan las VISTAS(carpeta /views) y para esto se usan TEMPLATES ENGINES que son MOTORES DE PLANTILLAS consultar:
-https://www.youtube.com/watch?v=1cHUZQFb4bo&list=PLPl81lqbj-4IEnmCXEJeEXPepr8gWtsl6&index=8
-Para renderizar la pagina se usa un framework EJS(Embedded Java Script) que es un motor de plantillas
-EJS mezcla HTML con JS
-*/
 
-/*Middlewares es un concepto muy importante al desarrollar sobre Node.js, 
-  la plataforma para utilizar a javascript en el lado del servidor.
-  Un middleware es un bloque de código que se ejecuta entre la petición que hace
-  el usuario (request) hasta que la petición llega al servidor.
-  Los middelware siempres estan antes de las peticiones del cliente
-
-  */
 //Las rutas que estaban inicialmete aqui se pasaron a la carpeta router archivo rutasWeb.js
 
 app.use('/', require('./router/rutasWeb'));
